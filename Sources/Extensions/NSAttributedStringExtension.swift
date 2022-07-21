2c49b7df30ebb3e19d65fa4464bee86154ffedd9
Irshad Ahmad <ahmedirshad281@gmail.com>
2022-07-21 14:11:41 +0530

    Updated the liscence info

--- Sources/Extensions/NSAttributedStringExtension.swift
+++ Sources/Extensions/NSAttributedStringExtension.swift
@@ -4,12 +4,38 @@
 //
 //  Created by Irshad Ahmad on 28/05/22.
 //
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import UIKit
 
 public extension NSAttributedString {
-
-    convenience init(htmlString html: String, font: UIFont? = nil, useDocumentFontSize: Bool = true) throws {
+    
+    /// - Parameters:
+    ///   - html: html string which needs to be convert
+    ///   - font: UIFont which needs to be set on `NSAttributedString`
+    ///   - useDocumentFontSize: pass true if want to use the default font
+    convenience init(
+        htmlString html: String,
+        font: UIFont? = nil,
+        useDocumentFontSize: Bool = true) throws
+    {
         let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
             .documentType: NSAttributedString.DocumentType.html,
             .characterEncoding: String.Encoding.utf8.rawValue
